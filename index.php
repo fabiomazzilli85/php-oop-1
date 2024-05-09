88691d42baee0dae8299d31ce9a237a314f80377
fabiomazzilli85 <155073103+fabiomazzilli85@users.noreply.github.com>
2024-05-09 16:28:20 +0200

    Add files via upload
    Ho creato una index.php. Ho creato una classe Production con tre istanze (titolo, lingua e voto). Creato costruttore. Lavorato con set / get.
    Creata e stampata a schermo production1. OK

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+class Production
+{
+    public $titolo;
+    public $lingua;
+    public $voto;
+    // Queste sono le istanze: titolo, lingua, voto.
+
+    public function __construct($titolo, $lingua, $voto) {
+        $this->titolo = $titolo;
+        $this->lingua = $lingua;
+        $this->voto = $voto;
+    }
+    // Questo è il costruttore.
+
+    public function setTitolo($titolo)
+    {
+        $this->titolo = $titolo;
+    }
+    // Imposto il titolo del film
+
+    public function getTitolo()
+    {
+        return $this->titolo;
+    }
+    // Ottengo il titolo del film
+
+    public function setLingua($lingua)
+    {
+        $this->lingua = $lingua;
+    }
+    // Imposto la lingua del film
+    public function getLingua()
+    {
+        return $this->lingua;
+    }
+    // Ottengo la lingua del film.
+
+    public function setVoto($voto)
+    {
+        if ($voto >= 1 && $voto <= 10) {
+            $this->voto = $voto;
+        } else {
+            echo "Mi dispiace ma il voto deve essere compreso tra 1 e 10.";
+        }
+    }
+    // Imposto il voto del film e con la logica IF / ELSE mi accerto che venga restituito il voto se esso è compreso tra 1 e 10, altrimenti stampo la scritta.
+
+    public function getVoto()
+    {
+        return $this->voto;
+    }
+    // Ottengo il voto del film.
+}
+
+$production1 = new Production("Il Padrino", "Inglese", 10);
+echo "Titolo: " . $production1->getTitolo() . "<br>";
+echo "Lingua: " . $production1->getLingua() . "<br>";
+echo "Voto: " . $production1->getVoto() . "<br>";
+
+?>
